524d7c335d6329d37f68911503b9706502b1d21f
RajAditya01 <adityavishal903@gmail.com>
2023-07-24 18:28:57 +0530

    schema model

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/aditya", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connection successful........."))
     .catch((err) => console.log(err));
+
+// Schema
+
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+// Model
+const Playlist = mongoose.model("Playlist", playlistSchema);

ac207530b82b9fa0f4089978195b532ba77a2a47
RajAditya01 <adityavishal903@gmail.com>
2023-07-24 22:19:04 +0530

    .

--- app.js
+++ app.js
@@ -1,40 +1,85 @@
+// const mongoose = require("mongoose");
+
+// mongoose
+//     .connect("mongodb://localhost:27017/aditya", {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("Connection successful........."))
+//     .catch((err) => console.log(err));
+
+// // Schema
+
+// const playlistSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true,
+//     },
+//     ctype: String,
+//     videos: Number,
+//     author: String,
+//     active: Boolean,
+
+//     date: {
+//         type: Date,
+//         default: Date.now,
+//     },
+// });
+
+// // collection and creation
+// const Playlist = mongoose.model("Playlist", playlistSchema);
+
+// const reactPlaylist = new Playlist({
+//     name: "Reactjs",
+//     ctype: "frontend",
+//     videos: 80,
+//     author: "Aditya",
+//     active: true,
+// });
+
+// reactPlaylist.save();
+
+
 const mongoose = require("mongoose");
 
-mongoose
-    .connect("mongodb://localhost:27017/aditya", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connection successful........."))
-    .catch((err) => console.log(err));
+(async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/aditya", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
-// Schema
+        console.log("Connection successful.........");
 
-const playlistSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    ctype: String,
-    videos: Number,
-    author: String,
-    active: Boolean,
+        const playlistSchema = new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+            },
+            ctype: String,
+            videos: Number,
+            author: String,
+            active: Boolean,
+            date: {
+                type: Date,
+                default: Date.now,
+            },
+        });
 
-    date: {
-        type: Date,
-        default: Date.now,
-    },
-});
+        const Playlist = mongoose.model("Playlist", playlistSchema);
 
-// collection and creation
-const Playlist = mongoose.model("Playlist", playlistSchema);
+        const reactPlaylist = new Playlist({
+            name: "Reactjs",
+            ctype: "frontend",
+            videos: 80,
+            author: "Aditya",
+            active: true,
+        });
 
-const reactPlaylist = new Playlist({
-    name: "Reactjs",
-    ctype: "frontend",
-    videos: 80,
-    author: "Aditya",
-    active: true,
-});
+        await reactPlaylist.save();
+        console.log("Playlist saved successfully!");
+    } catch (err) {
+        console.error("Error occurred:", err);
+    }
+})();
 
-reactPlaylist.save();

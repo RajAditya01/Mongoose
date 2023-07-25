ebc381b8e4e684ac904ef9e24e98a99b3b9ef2d8
RajAditya01 <adityavishal903@gmail.com>
2023-07-25 23:05:06 +0530

    .

--- app.js
+++ app.js
@@ -40,6 +40,94 @@
 // reactPlaylist.save();
 
 
+
+
+
+// const mongoose = require("mongoose");
+
+// (async () => {
+//     try {
+//         await mongoose.connect("mongodb://127.0.0.1:27017/aditya", {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+
+//         console.log("Connection successful.........");
+
+//         const playlistSchema = new mongoose.Schema({
+//             name: {
+//                 type: String,
+//                 required: true,
+//             },
+//             ctype: String,
+//             videos: Number,
+//             author: String,
+//             active: Boolean,
+//             date: {
+//                 type: Date,
+//                 default: Date.now,
+//             },
+//         });
+
+//         const Playlist = mongoose.model("Playlist", playlistSchema);
+
+//         const reactPlaylist = new Playlist({
+//             name: "Reactjs",
+//             ctype: "frontend",
+//             videos: 80,
+//             author: "Aditya",
+//             active: true,
+//         });
+
+//         const expressPlaylist = new Playlist({
+//             name: "Expressjs",
+//             ctype: "Backend",
+//             videos: 80,
+//             author: "Aditya",
+//             active: true,
+//         });
+
+//         const MongoDb = new Playlist({
+//             name: "MongoDb",
+//             ctype: "Database",
+//             videos: 90,
+//             author: "Aditya",
+//             active: true,
+//         });
+
+//         const Nodejs = new Playlist({
+//             name: "Nodejs",
+//             ctype: "Backend",
+//             videos: 180,
+//             author: "Aditya",
+//             active: true,
+//         });
+
+
+//         const result =await Playlist.insertMany([reactPlaylist,expressPlaylist,MongoDb,Nodejs]);
+
+//         //await reactPlaylist.save();
+//         console.log("Playlist saved successfully!");
+//     } catch (err) {
+//         console.error("Error occurred:", err);
+//     }
+// })();
+
+//         //createDocument();
+
+//         // Call the getDocument() function inside the IIFE to ensure it executes after document insertion
+//         const getDocument = async () => {
+//             const result = await Playlist.find();
+//             console.log(result);
+//         };
+//         getDocument();
+
+
+
+
+
+
+
 const mongoose = require("mongoose");
 
 (async () => {
@@ -52,22 +140,12 @@ const mongoose = require("mongoose");
         console.log("Connection successful.........");
 
         const playlistSchema = new mongoose.Schema({
-            name: {
-                type: String,
-                required: true,
-            },
-            ctype: String,
-            videos: Number,
-            author: String,
-            active: Boolean,
-            date: {
-                type: Date,
-                default: Date.now,
-            },
+            // ... schema definition ...
         });
 
         const Playlist = mongoose.model("Playlist", playlistSchema);
 
+        // Move the document creation inside the try block
         const reactPlaylist = new Playlist({
             name: "Reactjs",
             ctype: "frontend",
@@ -100,12 +178,18 @@ const mongoose = require("mongoose");
             active: true,
         });
 
-        const result =await Playlist.insertMany([reactPlaylist,expressPlaylist,MongoDb,Nodejs]);
+        const result = await Playlist.insertMany([reactPlaylist, expressPlaylist, MongoDb, Nodejs]);
 
-        //await reactPlaylist.save();
         console.log("Playlist saved successfully!");
+
+        // Call the getDocument() function inside the IIFE to ensure it executes after document insertion
+        const getDocument = async () => {
+            const result = await Playlist.find();
+            console.log(result);
+        };
+        getDocument();
+
     } catch (err) {
         console.error("Error occurred:", err);
     }
 })();
-
